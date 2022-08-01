0a536b012278a047e0af5397bcff91e21bdccd30
Leo-Kw <374545329@qq.com>
2022-08-02 00:29:12 +0800

    fix: :poop:

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,14 +2,25 @@ import React from 'react'
 
 export const LoginView: React.FC = () => {
   return (
-    <button
-      bg='blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600'
-      text='sm white'
-      font='mono light'
-      p='y-2 x-4'
-      border='2 rounded blue-200'
-    >
-      登录
-    </button>
+    <>
+      <button
+        bg='blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600'
+        text='sm white'
+        font='mono light'
+        p='y-2 x-4'
+        border='2 rounded blue-200'
+      >
+        登录
+      </button>
+      <button
+        bg='blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600'
+        text='sm white'
+        font='mono light'
+        p='y-2 x-4'
+        border='2 rounded blue-200'
+      >
+        登录
+      </button>
+    </>
   )
 }
