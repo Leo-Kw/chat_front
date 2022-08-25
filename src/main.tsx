830e4e58ee22e5f6593d47d78e535755579f4ffb
Leo-Kw <374545329@qq.com>
2022-08-25 23:28:17 +0800

    feat: change language

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,20 @@ import App from './App'
 import './index.css'
 import intl from 'react-intl-universal'
 import { Locale } from './shared/services'
-import { Zh, En } from './locale'
+// import { Zh, En } from './locale'
 import { getLocaleService } from './shared/services'
 import { notice } from './common/components/toast'
 
 // 用来注入 Windi CSS 所需的样式，一定要加上!
 import 'virtual:windi.css'
 
-intl.init({
-  currentLocale: getLocaleService(),
-  locales: {
-    [Locale.ZH]: Zh,
-    [Locale.EN]: En,
-  },
-})
+// intl.init({
+//   currentLocale: getLocaleService(),
+//   locales: {
+//     [Locale.ZH]: Zh,
+//     [Locale.EN]: En,
+//   },
+// })
 
 // 初始化Toast
 notice({ content: '', type: 'info', duration: 3000 })
