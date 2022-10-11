382e697644282c11aa415fa1470b4353e580a7cb
Leo <zhengruihang@rayvision.com>
2022-10-11 16:18:03 +0800

    :wrench: test pre-commit

--- src/shared/services/api/chat.ts
+++ src/shared/services/api/chat.ts
@@ -3,7 +3,7 @@ import { AxiosInstance } from 'axios'
 export class ChatModule {
   constructor(private ins: AxiosInstance) {}
 
-  test = async (parmas: any) => {
+  test = async (parmas: string) => {
     const res = await this.ins.post('/api/chat/test', parmas)
     return res.data
   }
