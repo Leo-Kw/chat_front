9447cdb1cfd04e3912472ba5f43f9f7620950927
Leo <zhengruihang@rayvision.com>
2022-10-26 17:27:16 +0800

    :green_heart: fix ci

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -2,13 +2,13 @@
  * @desc 节流 对高频事件的优化  如 scroll。
  * 与防抖区别：防抖在连续的操作过程中只触发一次，而节流可能触发多次，但是频率变低了。
  */
-export const throttle = (fun: () => void, delay = 300) => {
-  let timeout: any = null
+export const throttle = (func: () => void, delay = 300) => {
+  let timeout: number | null = null
   return function () {
     if (!timeout) {
-      timeout = setTimeout(function () {
+      timeout = window.setTimeout(function () {
         timeout = null
-        fun()
+        func()
       }, delay)
     }
   }
