6f661fe8e04c0182af6515b33fa58a00316a539f
Leo <zhengruihang@rayvision.com>
2023-10-10 19:32:32 +0800

    :sparkles: delete windcss .d.ts

--- src/types/shims.d.ts
+++
@@ -1,6 +0,0 @@
-import { AttributifyAttributes } from 'windicss/types/jsx'
-import React = require('react')
-
-declare module 'react' {
-  interface HTMLAttributes<T> extends AttributifyAttributes {}
-}
