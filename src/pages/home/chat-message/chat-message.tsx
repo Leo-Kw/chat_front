ea55d747ed2a6ddf7a438229cefbd0518aab42d7
Leo <zhengruihang@rayvision.com>
2023-10-13 23:18:15 +0800

    :bug: fix avatar bug

--- src/pages/home/chat-message/chat-message.tsx
+++ src/pages/home/chat-message/chat-message.tsx
@@ -1,10 +1,11 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { useAPI, useGlobalState, useIntlLocale, useSocket } from '@/hook'
-import { MessageTypes, SocketOnMessage } from '../type'
-import { scorllToBottom, substringByByte, throttle } from '@/utils'
+import { SocketOnMessage } from '../type'
+import { scorllToBottom, throttle } from '@/utils'
 import { ActionType } from '@/context'
-import { Icon, Toast } from '@/common/components'
+import { Toast } from '@/common/components'
 import { CSSTransition } from 'react-transition-group'
+import { MessageItem } from './message-item'
 
 export const ChatMessage = () => {
   const API = useAPI()
@@ -100,49 +101,7 @@ export const ChatMessage = () => {
       >
         {messageList.map((item) => {
           const isMyself = item.userInfo.id === userInfo.id
-          let imgLoadSuccess = true
-          return (
-            <div
-              key={item.id}
-              id={`${item.id}`}
-              className={`flex ${
-                isMyself ? 'justify-end' : 'justify-start'
-              } my-[10px] text-text-lighter`}
-            >
-              <div className={`flex ${isMyself ? 'flex-row-reverse' : 'flex-row'}`}>
-                <div className='w-[50px] h-[50px] rounded-[5px] flex justify-center items-center bg-gray-200 overflow-hidden [&>img]:w-full [&>img]:h-full [&>img]:object-contain'>
-                  {item.userInfo.avatar && imgLoadSuccess ? (
-                    <img
-                      src={item.userInfo.avatar}
-                      alt='avatar'
-                      onLoad={() => (imgLoadSuccess = true)}
-                      onError={() => (imgLoadSuccess = false)}
-                    />
-                  ) : (
-                    <Icon type='default-avatar' style={{ width: '35px', height: '35px' }} />
-                  )}
-                </div>
-                <div
-                  className={`flex flex-col ${isMyself ? 'items-end' : 'items-start'} mx-[15px]`}
-                >
-                  <div className='mb-[5px] text-[14px] text-textSa text-ellipsis overflow-hidden max-w-[200px]'>
-                    {item.userInfo.name}
-                  </div>
-                  <div
-                    className={`text-dark-main rounded-[5px] max-w-[350px] py-[5px] px-[15px] h-auto relative break-all before:content-[''] before:border-solid before:border-transparent before:h-0 before:absolute before:w-0 before:border-[5px] before:top-[12px] ${
-                      isMyself
-                        ? 'bg-messageBackground-myself before:left-full before:border-l-messageBackground-myself'
-                        : 'bg-messageBackground-other before:right-full before:border-r-messageBackground-other'
-                    }`}
-                  >
-                    {item.messageType === MessageTypes.HasEmoji
-                      ? substringByByte(item.messageContent, 100000)
-                      : item.messageContent}
-                  </div>
-                </div>
-              </div>
-            </div>
-          )
+          return <MessageItem key={item.id} item={item} isMyself={isMyself} />
         })}
         <div id='messageBottom' />
       </div>
